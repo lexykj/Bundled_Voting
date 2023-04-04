d40ac11c6887ebe7d242fc860928703c49fe05c6
CyanYeti <3tferney@gmail.com>
2023-04-03 21:38:07 -0600

    Added start of max bundle size bundler

--- voting/Bundler.java
+++ voting/Bundler.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Bundler {
     private ArrayList<Item> Shows; // this could potentially be removed
     private final ArrayList<Bundle> Bundles = new ArrayList<>();
+    private Random rnd = new Random();
     public Bundler(ArrayList<Item> shows) {
         this.Shows = (ArrayList<Item>) shows.clone();
         //Testing code with 3 items
@@ -11,18 +13,46 @@ public class Bundler {
 //        this.Shows.add(shows.get(1));
 //        this.Shows.add(shows.get(2));
 
+        rnd.setSeed(11111111);
+
         // CREATE bundles
         for (int i = 0; i < this.Shows.size(); i++){
-            rc(0, new ArrayList<Item>(), 3,i);
+//            rc(0, new ArrayList<Item>(), 4,i);
+            rcRandomWithMaxBundles(0, new ArrayList<Item>(), 4,i, 1000);
         }
 
-//        for (Bundle bundle : this.Bundles) {
-//            System.out.println("Bundle");
-//            System.out.println(bundle.toString());
-//        }
+        for (Bundle bundle : this.Bundles) {
+            System.out.println("Bundle");
+            System.out.println(bundle.toString());
+        }
         System.out.println("Shows: " + this.Shows.size());
         System.out.println("Bundled: "+this.Bundles.size());
     }
+
+
+    private void rcRandomWithMaxBundles(int depth, ArrayList<Item> curr, int size, int me, int MaxBundlesSize) {
+        if (depth >= size) return; // Should be useless
+        if (this.Bundles.size() >= MaxBundlesSize) return;
+        if (depth == size-1) { // If we are at the max bundle size add me
+//            System.out.println(depth + " " + size);
+            if (!curr.contains(this.Shows.get(me))) {
+
+                curr.add(this.Shows.get(me));
+                Bundle tempBundle = new Bundle(curr, ""+getNextId());
+                if (!this.Bundles.contains(tempBundle)) {
+                    this.Bundles.add(tempBundle);
+                }
+            }
+            return;
+        }
+        if (!curr.contains(this.Shows.get(me))) { // mid way if not me add me
+            curr.add(this.Shows.get(me));
+            for(int i = me; i < this.Shows.size(); i++) {
+                rcRandomWithMaxBundles(depth+1, new ArrayList<>(curr), size, rnd.nextInt(this.Shows.size()), MaxBundlesSize);
+            }
+        }
+    }
+
 
     private void rc(int depth, ArrayList<Item> curr, int size, int me) {
         if (depth >= size) return; // Should be useless
